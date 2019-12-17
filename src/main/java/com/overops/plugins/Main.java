5be1ad0713a779746f2e359ab371948ff9691840
Volodymyr Rachuk <rachukvolodymyr@gmail.com>
2019-12-17 22:44:18 +0200

    INTG-301: Concourse log4j binding issue

--- src/main/java/com/overops/plugins/Main.java
+++ src/main/java/com/overops/plugins/Main.java
@@ -1,8 +1,11 @@
 package com.overops.plugins;
 
 
+import org.apache.log4j.BasicConfigurator;
+
 public class Main {
     public static void main(String[] args) {
+        BasicConfigurator.configure();
         ConcoursePlugin.run(args);
     }
 }
