095986f7c4d824341d19d7bce9968d0ea45e581c
vitaliimaz <vitalik.mahara@gmail.com>
2020-02-10 14:32:18 +0200

    add common rule for new, critical, resurfaced and increasing errors reporting

--- src/main/java/com/overops/plugins/service/impl/RenderService.java
+++ src/main/java/com/overops/plugins/service/impl/RenderService.java
@@ -1,23 +1,23 @@
 package com.overops.plugins.service.impl;
 
 import com.overops.plugins.Context;
-import com.overops.plugins.model.OOReportYaml;
-import com.overops.plugins.model.QualityGateSummaryYaml;
-import com.overops.plugins.model.SummaryRow;
-import com.overops.plugins.model.YamlObject;
+import com.overops.plugins.model.*;
 import com.overops.plugins.service.Render;
 import com.takipi.api.client.util.cicd.OOReportEvent;
 import org.fusesource.jansi.Ansi;
 import com.overops.plugins.service.OutputWriter;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class RenderService extends Render {
 
     private ReportBuilder.QualityReport qualityReport;
     private OutputWriter outputStream;
+    private QualityGate criticalQualityGate;
+    private QualityGate newQualityGate;
+    private QualityGate resurfacedQualityGate;
+    private QualityGate increasingQualityGate;
 
     public RenderService(Context context, ReportBuilder.QualityReport report) {
         super(context);
@@ -43,40 +43,18 @@ public class RenderService extends Render {
 
     @Override
     public void render() {
-        if (getMarkedUnstable() && getUnstable()) {
-            outputStream.error(getSummary());
-        } else if (getMarkedUnstable() && !getUnstable()) {
-            outputStream.success(getSummary());
-        } else if (!getMarkedUnstable() && getUnstable()) {
-            outputStream.yellowFgPrintln(getSummary());
-        } else {
-            outputStream.success(getSummary());
-        }
+        initQualityGates();
 
-        printSeparator();
+        printMainQualityGateStatusSection();
+        printQualityGatesSummarySection();
+        printQualityGateSection(newQualityGate);
+        printQualityGateSection(resurfacedQualityGate);
+        printTotalUniqueErrorsSection();
+        printQualityGateSection(criticalQualityGate);
+        printQualityGateSection(increasingQualityGate);
+    }
 
-        printQualityGatesSummary();
-
-        printSeparator();
-
-        if (getCheckNewEvents() && getPassedNewErrorGate()) {
-            outputStream.printStatementHeaders( getNewErrorSummary());
-        } else if (getCheckNewEvents() && !getPassedNewErrorGate()) {
-            outputStream.printStatementHeaders(getNewErrorSummary());
-            outputStream.printYamlObject(new OOReportYaml(getNewEvents()));
-        }
-
-        printSeparator();
-
-        if (getCheckResurfacedEvents() && getPassedResurfacedErrorGate()) {
-            outputStream.printStatementHeaders( getResurfacedErrorSummary());
-        } else if (getCheckResurfacedEvents() && !getCheckResurfacedEvents()) {
-            outputStream.printStatementHeaders(getResurfacedErrorSummary());
-            outputStream.printYamlObject(new OOReportYaml(getResurfacedEvents()));
-        }
-
-        printSeparator();
-
+    private void printTotalUniqueErrorsSection() {
         if (getCheckTotalErrors() || getCheckUniqueErrors()) {
             if (getCheckTotalErrors() && getPassedTotalErrorGate()) {
                 outputStream.printStatementHeaders(getTotalErrorSummary());
@@ -94,33 +72,78 @@ public class RenderService extends Render {
                 outputStream.printYamlObject(new OOReportYaml(getTopEvents()));
             }
         }
-
         printSeparator();
+    }
 
-        if (getCheckCriticalErrors() && getPassedCriticalErrorGate()) {
-            outputStream.printStatementHeaders( getCriticalErrorSummary());
-        } else if (getCheckCriticalErrors() && !getPassedCriticalErrorGate()) {
-            outputStream.printStatementHeaders(getCriticalErrorSummary());
-            outputStream.printYamlObject(new OOReportYaml(getCriticalEvents()));
+    private void initQualityGates() {
+        newQualityGate = new QualityGate.Builder()
+                .setGateName("New Error")
+                .setErrorsName("new errors")
+                .setSummaryGateName("New")
+                .setIncludeIntoReport(qualityReport.isCheckNewGate())
+                .setReportCollection(qualityReport.getNewIssues())
+                .build();
+
+        resurfacedQualityGate = new QualityGate.Builder()
+                .setGateName("Resurfaced Error")
+                .setErrorsName("resurfaced errors")
+                .setSummaryGateName("Resurfaced")
+                .setIncludeIntoReport(qualityReport.isCheckResurfacedGate())
+                .setReportCollection(qualityReport.getResurfacedErrors())
+                .build();
+
+        criticalQualityGate = new QualityGate.Builder()
+                .setGateName("Critical Error")
+                .setErrorsName("critical errors")
+                .setSummaryGateName("Critical")
+                .setIncludeIntoReport(qualityReport.isCheckCriticalGate())
+                .setReportCollection(qualityReport.getCriticalErrors())
+                .build();
+
+        increasingQualityGate = new QualityGate.Builder()
+                .setGateName("Increasing Error")
+                .setPassedSummary("Increasing Quality Gate: Passed, OverOps did not detect any increasing errors in the current build against the baseline of " + qualityReport.getInput().baselineTime + ".")
+                .setNotPassedSummary("Increasing Quality Gate: Failed, OverOps detected increasing errors in the current build against the baseline of " + qualityReport.getInput().baselineTime + ".")
+                .setSummaryGateName("Increasing")
+                .setIncludeIntoReport(qualityReport.isCheckRegressionGate())
+                .setReportCollection(qualityReport.getRegressions())
+                .build();
+    }
+
+    private void printMainQualityGateStatusSection() {
+        if (getMarkedUnstable() && getUnstable()) {
+            outputStream.error(getSummary());
+        } else if (getMarkedUnstable() && !getUnstable()) {
+            outputStream.success(getSummary());
+        } else if (!getMarkedUnstable() && getUnstable()) {
+            outputStream.yellowFgPrintln(getSummary());
+        } else {
+            outputStream.success(getSummary());
+        }
+        printSeparator();
+    }
+
+    private void printQualityGateSection(QualityGate qualityGate) {
+        YamlObject qualityGateYamlRepresentation = new OOReportYaml(qualityGate.getReportCollection());
+        if (qualityGate.isIncludeIntoReport()) {
+            if (qualityGate.passed()) {
+                outputStream.printStatementHeaders(qualityGate.getSummary());
+            } else {
+                outputStream.printStatementHeaders(qualityGate.getSummary());
+                outputStream.printYamlObject(qualityGateYamlRepresentation);
+            }
         }
 
         printSeparator();
-
-        if (getCheckRegressedErrors() && getPassedRegressedEvents()) {
-            outputStream.printStatementHeaders( getRegressionSumarry());
-        } else if (getCheckRegressedErrors() && !getPassedRegressedEvents()) {
-            outputStream.printStatementHeaders(getRegressionSumarry());
-            outputStream.printYamlObject(new OOReportYaml(getRegressedEvents()));
-        }
     }
 
     private void printSeparator() {
         outputStream.yellowFgPrintln("");
     }
 
-    private void printQualityGatesSummary() {
+    private void printQualityGatesSummarySection() {
         outputStream.printStatementHeaders("Report Summary");
-        outputStream.printYamlObject(getSummaryCollection(), (property, value) -> {
+        outputStream.printYamlObject(getSummaryYamlObject(), (property, value) -> {
             if (property.equals("status")) {
                 if (value.equals("Passed")) {
                     return Ansi.Color.GREEN;
@@ -131,19 +154,18 @@ public class RenderService extends Render {
 
             return null;
         });
+        printSeparator();
     }
 
-    private YamlObject getSummaryCollection() {
+    private YamlObject getSummaryYamlObject() {
         QualityGateSummaryYaml qualityGateSummaryYaml = new QualityGateSummaryYaml("gates");
-        final String passedString =  "-";
-        boolean passedNewErrorGate = getPassedNewErrorGate();
-        boolean passedResurfacedErrorGate = getPassedResurfacedErrorGate();
-        boolean passedCriticalErrorGate = getPassedCriticalErrorGate();
-        boolean passedRegressedEvents = getPassedRegressedEvents();
-        qualityGateSummaryYaml.addSummaryRow(new SummaryRow("New", passedNewErrorGate, passedNewErrorGate ? passedString : String.valueOf(getNewEvents().size())));
-        qualityGateSummaryYaml.addSummaryRow(new SummaryRow("Resurfaced", passedResurfacedErrorGate, passedResurfacedErrorGate ? passedString : String.valueOf(getResurfacedEvents().size())));
-        qualityGateSummaryYaml.addSummaryRow(new SummaryRow("Critical", passedCriticalErrorGate, passedCriticalErrorGate ? passedString : String.valueOf(getCriticalEvents().size())));
-        qualityGateSummaryYaml.addSummaryRow(new SummaryRow("Increasing", passedRegressedEvents, passedRegressedEvents ? passedString : String.valueOf(getResurfacedEvents().size())));
+
+        Stream.of(newQualityGate, resurfacedQualityGate, criticalQualityGate, increasingQualityGate).forEach( qualityGate -> {
+            if (qualityGate.isIncludeIntoReport()) {
+                qualityGateSummaryYaml.addSummaryRow(new SummaryRow(qualityGate));
+            }
+        });
+
         return qualityGateSummaryYaml;
     }
 
@@ -158,10 +180,10 @@ public class RenderService extends Render {
     private String getSummary() {
         if (getUnstable() && getMarkedUnstable()) {
             //the build is unstable when marking the build as unstable
-            return "OverOps has marked build "+ getDeploymentName() + " as unstable because the below quality gate(s) were not met.";
+            return "OverOps has marked build " + getDeploymentName() + " as unstable because the below quality gate(s) were not met.";
         } else if (!getMarkedUnstable() && getUnstable()) {
             //unstable build stable when NOT marking the build as unstable
-            return "OverOps has detected issues with build "+ getDeploymentName() + " but did not mark the build as unstable.";
+            return "OverOps has detected issues with build " + getDeploymentName() + " but did not mark the build as unstable.";
         } else {
             //stable build when marking the build as unstable
             return "Congratulations, build " + getDeploymentName() + " has passed all quality gates!";
@@ -173,92 +195,6 @@ public class RenderService extends Render {
         value = value.replace("[", "");
         value = value.replace("]", "");
         return value;
-    }
-
-    private boolean getPassedNewErrorGate() {
-        return getCheckNewEvents() && !getNewErrorsExist();
-
-    }
-
-    private boolean getCheckNewEvents() {
-        return qualityReport.isCheckNewGate();
-    }
-
-    private String getNewErrorSummary() {
-        if (getNewEvents() != null && getNewEvents().size() > 0) {
-            return "New Error Gate: Failed, OverOps detected " + qualityReport.getNewIssues().size() + " new error(s) in your build.";
-        } else if (qualityReport.isCheckNewGate()) {
-            return "New Error Gate: Passed, OverOps did not detect any new errors in your build.";
-        }
-
-        return null;
-    }
-
-    private boolean getNewErrorsExist() {
-        return getNewEvents() != null && getNewEvents().size() > 0;
-    }
-
-    private List<OOReportEvent> getNewEvents() {
-        return qualityReport.getNewIssues();
-    }
-
-    private boolean getPassedResurfacedErrorGate() {
-        return getCheckResurfacedEvents() && !getResurfacedErrorsExist();
-
-    }
-
-    private boolean getResurfacedErrorsExist() {
-        return getResurfacedEvents() != null && getResurfacedEvents().size() > 0;
-    }
-
-    private boolean getCheckResurfacedEvents() {
-        return qualityReport.isCheckResurfacedGate();
-    }
-
-    private String getResurfacedErrorSummary() {
-        if (getResurfacedEvents() != null && getResurfacedEvents().size() > 0) {
-            return "Resurfaced Error Gate: Failed, OverOps detected " + qualityReport.getResurfacedErrors().size() + " resurfaced errors in your build.";
-        } else if (qualityReport.isCheckResurfacedGate()) {
-            return "Resurfaced Error Gate: Passed, OverOps did not detect any resurfaced errors in your build.";
-        }
-
-        return null;
-    }
-
-    private List<OOReportEvent> getResurfacedEvents() {
-        return qualityReport.getResurfacedErrors();
-    }
-
-    private boolean getCheckCriticalErrors() {
-        return qualityReport.isCheckCriticalGate();
-    }
-
-    private boolean getPassedCriticalErrorGate() {
-        return getCheckCriticalErrors() && !getCriticalErrorsExist();
-
-    }
-
-    private boolean getCriticalErrorsExist() {
-        return getCriticalEvents() != null && getCriticalEvents().size() > 0;
-    }
-
-    private String getCriticalErrorSummary() {
-        if (getCriticalEvents() != null && getCriticalEvents().size() > 0) {
-            return "Critical Error Gate: Failed, OverOps detected " + qualityReport.getCriticalErrors().size() + " critical errors in your build.";
-        } else if (qualityReport.isCheckCriticalGate()) {
-            return "Critical Error Gate: Passed, OverOps did not detect any critical errors in your build.";
-        }
-
-        return null;
-    }
-
-    private List<OOReportEvent> getCriticalEvents() {
-        return qualityReport.getCriticalErrors();
-    }
-
-    //this will serve as a check for either unique or total error gates
-    private boolean getCountGates() {
-        return getCheckUniqueErrors() || getCheckTotalErrors();
     }
 
     private boolean getCheckTotalErrors() {
@@ -305,27 +241,5 @@ public class RenderService extends Render {
 
     private List<OOReportEvent> getTopEvents() {
         return qualityReport.getTopErrors();
-    }
-
-    private String getRegressionSumarry() {
-        if (!getPassedRegressedEvents()) {
-            return "Increasing Quality Gate: Failed, OverOps detected incressing errors in the current build against the baseline of " + qualityReport.getInput().baselineTime;
-        } else if (getPassedRegressedEvents()) {
-            return "Increasing Quality Gate: Passed, OverOps did not detect any increasing errors in the current build against the baseline of " + qualityReport.getInput().baselineTime;
-        }
-
-        return null;
-    }
-
-    private boolean getCheckRegressedErrors() {
-        return qualityReport.isCheckRegressionGate();
-    }
-
-    private boolean getPassedRegressedEvents() {
-        return !getCheckRegressedErrors() || qualityReport.getRegressions() == null || qualityReport.getRegressions().size() <= 0;
-    }
-
-    private List<OOReportEvent> getRegressedEvents() {
-        return qualityReport.getAllIssues();
     }
 }
--- src/main/java/com/overops/plugins/service/impl/ReportBuilder.java
+++ src/main/java/com/overops/plugins/service/impl/ReportBuilder.java
@@ -15,7 +15,7 @@ import java.util.*;
 import java.util.regex.Pattern;
 
 public class ReportBuilder {
-	
+
 	public static class QualityReport {
 
 		private final List<OOReportEvent> newIssues;
@@ -40,26 +40,26 @@ public class ReportBuilder {
 		private final boolean markedUnstable;
 
 		protected QualityReport(RegressionInput input, RateRegression regression,
-			List<OOReportRegressedEvent> regressions, List<OOReportEvent> criticalErrors, 
+			List<OOReportRegressedEvent> regressions, List<OOReportEvent> criticalErrors,
 			List<OOReportEvent> topErrors, List<OOReportEvent> newIssues,
 			List<OOReportEvent> resurfacedErrors, long eventVolume, int uniqueEventCounts, boolean unstable,
 			boolean checkNewGate, boolean checkResurfacedGate, boolean checkCriticalGate, boolean checkVolumeGate,
 			boolean checkUniqueGate, boolean checkRegressionGate, Integer maxEventVolume, Integer maxUniqueVolume, boolean markedUnstable) {
-			
+
 			this.input = input;
 			this.regression = regression;
-			
+
 			this.regressions = regressions;
 			this.allIssues = new ArrayList<OOReportEvent>();
 			this.newIssues = newIssues;
 			this.criticalErrors = criticalErrors;
 			this.topErrors = topErrors;
 			this.resurfacedErrors = resurfacedErrors;
-			
+
 			if (regressions != null) {
 				allIssues.addAll(regressions);
 			}
-		
+
 			this.eventVolume =  eventVolume;
 			this.uniqueEventsCount =  uniqueEventCounts;
 			this.unstable = unstable;
@@ -73,7 +73,7 @@ public class ReportBuilder {
 			this.maxUniqueVolume = maxUniqueVolume;
 			this.markedUnstable = markedUnstable;
 		}
-		
+
 		public RegressionInput getInput() {
 			return input;
 		}
@@ -81,7 +81,7 @@ public class ReportBuilder {
 		public RateRegression getRegression() {
 			return regression;
 		}
-		
+
 		public List<OOReportEvent> getResurfacedErrors() {
 			return resurfacedErrors;
 		}
@@ -97,7 +97,7 @@ public class ReportBuilder {
 		public List<OOReportEvent> getTopErrors() {
 			return topErrors;
 		}
-		
+
 		public List<OOReportEvent> getNewIssues() {
 			return newIssues;
 		}
@@ -105,11 +105,11 @@ public class ReportBuilder {
 		public List<OOReportRegressedEvent> getRegressions() {
 			return regressions;
 		}
-		
+
 		public long getUniqueEventsCount() {
-			return uniqueEventsCount; 
+			return uniqueEventsCount;
 		}
-		
+
 		public boolean getUnstable() {
 			return unstable;
 		}
@@ -180,29 +180,29 @@ public class ReportBuilder {
 			return Arrays.asList(new Metadata("type", e.type), new Metadata("summary", e.summary),new Metadata("name", e.name));
 		}
 	}
-	
+
 	private static class ReportVolume {
 		protected List<OOReportEvent> topEvents;
 		protected Collection<EventResult> filter;
-		
+
 	}
-	
+
 	private static boolean allowEvent(EventResult event, Pattern pattern) {
-		
+
 		if (pattern == null ) {
 			return true;
 		}
-		
+
 		String json = new Gson().toJson(event);
 		boolean result = !pattern.matcher(json).find();
-		
+
 		return result;
 	}
-	
+
 	private static List<EventResult> getSortedEventsByVolume(Collection<EventResult> events) {
-		
+
 		List<EventResult> result = new ArrayList<EventResult>(events);
-		
+
 		result.sort((o1, o2) -> {
 			long v1;
 			long v2;
@@ -221,102 +221,102 @@ public class ReportBuilder {
 
 			return (int)(v2 - v1);
 		});
-		
+
 		return result;
 	}
-	
-	private static void addEvent(Set<EventResult> events, EventResult event, 
+
+	private static void addEvent(Set<EventResult> events, EventResult event,
 		Pattern pattern, PrintStream output, boolean verbose) {
-		
+
 		if (allowEvent(event, pattern)) {
 			events.add(event);
 		} else if ((output != null) && (verbose)) {
 			output.println(event + " did not match regexFilter and was skipped");
 		}
 	}
-	
+
 	private static Collection<EventResult> filterEvents(RateRegression rateRegression,
 			Pattern pattern, PrintStream output, boolean verbose) {
-		
+
 		Set<EventResult> result = new HashSet<>();
-		
+
 		if (pattern != null) {
-		
+
 			for (EventResult event : rateRegression.getNonRegressions()) {
 				addEvent(result, event, pattern, output, verbose);
 			}
-			
+
 			for (EventResult event : rateRegression.getAllNewEvents().values()) {
 				addEvent(result, event, pattern, output, verbose);
 			}
-			
+
 			for (RegressionResult regressionResult : rateRegression.getAllRegressions().values()) {
 				addEvent(result, regressionResult.getEvent(), pattern, output, verbose);
 
 			}
-			
+
 		} else {
 			result.addAll(rateRegression.getNonRegressions());
 			result.addAll(rateRegression.getAllNewEvents().values());
-		
+
 			for (RegressionResult regressionResult : rateRegression.getAllRegressions().values()) {
 				result.add(regressionResult.getEvent());
 			}
 		}
-		
+
 		return result;
 	}
-	
-	private static ReportVolume getReportVolume(ApiClient apiClient, 
+
+	private static ReportVolume getReportVolume(ApiClient apiClient,
 			RegressionInput input, RateRegression rateRegression,
 			int limit, String regexFilter,  PrintStream output, boolean verbose) {
-		
+
 		ReportVolume result = new ReportVolume();
-		
+
 		Pattern pattern;
-		
+
 		if ((regexFilter != null) && (regexFilter.length() > 0)) {
 			pattern = Pattern.compile(regexFilter);
 		} else {
 			pattern = null;
 		}
-		
-		Collection<EventResult> eventsSet = filterEvents(rateRegression, pattern, output, verbose);		
+
+		Collection<EventResult> eventsSet = filterEvents(rateRegression, pattern, output, verbose);
 		List<EventResult> events =  getSortedEventsByVolume(eventsSet);
-		
+
 		if (pattern != null) {
 			result.filter = eventsSet;
 		}
-				
+
 		result.topEvents = new ArrayList<>();
-				
-		for (EventResult event : events) {	
+
+		for (EventResult event : events) {
 			if (event.stats != null) {
 				if (result.topEvents.size() < limit) {
 					String arcLink = ProcessQualityGates.getArcLink(apiClient, event.id, input, rateRegression.getActiveWndowStart());
 					result.topEvents.add(new OOReportEvent(event, arcLink));
 				}
-			}		
+			}
 		}
-				
+
 		return result;
 	}
-	
+
 	/*
 	 * Entry point into report engine
 	 */
-	public static QualityReport execute(ApiClient apiClient, RegressionInput input, 
-			Integer maxEventVolume, Integer maxUniqueErrors, int topEventLimit, String regexFilter, 
+	public static QualityReport execute(ApiClient apiClient, RegressionInput input,
+			Integer maxEventVolume, Integer maxUniqueErrors, int topEventLimit, String regexFilter,
 			boolean newEvents, boolean resurfacedEvents, boolean runRegressions, boolean markedUnstable, PrintStream output, boolean verbose) {
-		
+
 		//check if total or unique gates are being tested
 		boolean countGate = false;
 		if (maxEventVolume != 0 || maxUniqueErrors != 0) {
 			countGate = true;
-		} 
+		}
 		boolean checkMaxEventGate = maxEventVolume != 0;
 		boolean checkUniqueEventGate = maxUniqueErrors != 0;
-		
+
 		boolean failedAPI = false;
 
 		//get the CICD quality report for all gates but Regressions
@@ -324,17 +324,17 @@ public class ReportBuilder {
 		QualityGateReport qualityGateReport = new QualityGateReport();
 		if (countGate || newEvents || resurfacedEvents || regexFilter != null) {
 			try {
-				qualityGateReport = ProcessQualityGates.processCICDInputs(apiClient, input, newEvents, resurfacedEvents, 
+				qualityGateReport = ProcessQualityGates.processCICDInputs(apiClient, input, newEvents, resurfacedEvents,
 					regexFilter, topEventLimit, countGate, output, verbose);
 			} catch (Exception e) {
-				if (output != null) { 
+				if (output != null) {
 					output.println("Error processing CI CD inputs " + e.getMessage());
 				}
 
 				failedAPI = true;
 			}
 		}
-		 
+
 		//run the regression gate
 		ReportVolume reportVolume;
 		RateRegression rateRegression = null;
@@ -342,20 +342,20 @@ public class ReportBuilder {
 		boolean hasRegressions = false;
 		if (runRegressions) {
 			rateRegression = RegressionUtil.calculateRateRegressions(apiClient, input, output, verbose);
-			
-			reportVolume = getReportVolume(apiClient, input, 
+
+			reportVolume = getReportVolume(apiClient, input,
 					rateRegression, topEventLimit, regexFilter, output, verbose);
-				
+
 			regressions = getAllRegressions(apiClient, input, rateRegression, reportVolume.filter);
 			if (regressions != null && regressions.size() > 0) {
 				hasRegressions = true;
 				replaceSourceId2(regressions);
 			}
 		}
-		
+
 		//max total error gate
 		boolean maxVolumeExceeded = (maxEventVolume != 0) && (qualityGateReport.getTotalErrorCount() > maxEventVolume);
-		
+
 		//max unique error gate
 		long uniqueEventCount;
 		boolean maxUniqueErrorsExceeded;
@@ -366,38 +366,38 @@ public class ReportBuilder {
 			uniqueEventCount = 0;
 			maxUniqueErrorsExceeded = false;
 		}
-		
+
 		//new error gate
 		boolean newErrors = false;
 		if (qualityGateReport.getNewErrors() != null && qualityGateReport.getNewErrors().size() > 0) {
 			newErrors = true;
 			replaceSourceId(qualityGateReport.getNewErrors());
 		}
-		
+
 		//resurfaced error gate
 		boolean resurfaced = false;
 		if (qualityGateReport.getResurfacedErrors() != null && qualityGateReport.getResurfacedErrors().size() > 0) {
 			resurfaced = true;
 			replaceSourceId(qualityGateReport.getResurfacedErrors());
 		}
-		
+
 		//critical error gate
 		boolean critical = false;
 		if (qualityGateReport.getCriticalErrors() != null  && qualityGateReport.getCriticalErrors().size() > 0) {
 			critical = true;
 			replaceSourceId(qualityGateReport.getCriticalErrors());
 		}
-		
+
 		//top errors
 		if (qualityGateReport.getTopErrors() != null  && qualityGateReport.getTopErrors().size() > 0) {
 			replaceSourceId(qualityGateReport.getTopErrors());
 		}
-		
+
 		boolean checkCritical = false;
 		if (input.criticalExceptionTypes != null && input.criticalExceptionTypes.size() > 0) {
 			checkCritical = true;
 		}
-		
+
 		boolean unstable = (failedAPI)
 				|| (hasRegressions)
 				|| (maxVolumeExceeded)
@@ -405,14 +405,14 @@ public class ReportBuilder {
 				|| (newErrors)
 				|| (resurfaced)
 				|| (critical);
-		
+
 		return new QualityReport(input, rateRegression, regressions,
-				qualityGateReport.getCriticalErrors(), qualityGateReport.getTopErrors(), qualityGateReport.getNewErrors(), 
+				qualityGateReport.getCriticalErrors(), qualityGateReport.getTopErrors(), qualityGateReport.getNewErrors(),
 				qualityGateReport.getResurfacedErrors(), qualityGateReport.getTotalErrorCount(),
 				qualityGateReport.getUniqueErrorCount(), unstable, newEvents, resurfacedEvents, checkCritical, checkMaxEventGate,
 				checkUniqueEventGate, runRegressions, maxEventVolume, maxUniqueErrors, markedUnstable);
 	}
-	
+
 	//for each event, replace the source ID in the ARC link with the number 4 (which means Jenkins)
 	private static void replaceSourceId (List<OOReportEvent> events) {
 		for (OOReportEvent ooReportEvent : events) {
@@ -420,7 +420,7 @@ public class ReportBuilder {
 			ooReportEvent.setArcLink(arcLink);
 		}
 	}
-	
+
 	//for each event, replace the source ID in the ARC link with the number 4 (which means Jenkins)
 	private static void replaceSourceId2 (List<OOReportRegressedEvent> events) {
 		for (OOReportEvent ooReportEvent : events) {
@@ -428,7 +428,7 @@ public class ReportBuilder {
 			ooReportEvent.setArcLink(arcLink);
 		}
 	}
-	
+
 	private static String replaceSourceIdInArcLink(String arcLink) {
 		if (arcLink == null) {
 			return arcLink;
@@ -436,13 +436,13 @@ public class ReportBuilder {
 		String returnString;
 		CharSequence target = "source=43";
 		CharSequence replacement = "source=4";
-				
+
 		returnString = arcLink.replace(target, replacement);
-		
-		return returnString;	
+
+		return returnString;
 	}
-		
-	private static List<OOReportRegressedEvent> getAllRegressions(ApiClient apiClient, 
+
+	private static List<OOReportRegressedEvent> getAllRegressions(ApiClient apiClient,
 			RegressionInput input, RateRegression rateRegression, Collection<EventResult> filter) {
 
 		List<OOReportRegressedEvent> result = new ArrayList<OOReportRegressedEvent>();
@@ -452,7 +452,7 @@ public class ReportBuilder {
 			if ((filter != null) && (!filter.contains(regressionResult.getEvent()))) {
 				continue;
 			}
-			
+
 			String arcLink = ProcessQualityGates.getArcLink(apiClient, regressionResult.getEvent().id, input, rateRegression.getActiveWndowStart());
 
 			OOReportRegressedEvent regressedEvent = new OOReportRegressedEvent(regressionResult.getEvent(),
@@ -460,13 +460,13 @@ public class ReportBuilder {
 
 			result.add(regressedEvent);
 		}
-		
+
 		for (RegressionResult regressionResult : rateRegression.getAllRegressions().values()) {
 
 			if (rateRegression.getCriticalRegressions().containsKey(regressionResult.getEvent().id)) {
 				continue;
 			}
-			
+
 			String arcLink = ProcessQualityGates.getArcLink(apiClient, regressionResult.getEvent().id, input, rateRegression.getActiveWndowStart());
 
 			OOReportRegressedEvent regressedEvent = new OOReportRegressedEvent(regressionResult.getEvent(),
