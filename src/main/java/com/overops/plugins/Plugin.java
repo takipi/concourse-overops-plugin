04e0f999509fe974a338103f4d5bd162f9778c3c
Brandon Groves <brandon.groves@gmail.com>
2020-05-15 13:46:28 -0400

    [OO-10272] Added the flag for passing the build on exception

--- src/main/java/com/overops/plugins/Plugin.java
+++ src/main/java/com/overops/plugins/Plugin.java
@@ -41,24 +41,47 @@ public class Plugin {
                             trace -> context.getOutputStream().println(trace, Ansi.Color.WHITE)
                     );
                 }
-
+    
                 // Check to make build stable or not
-                status = qualityReport.getStatusCode() == QualityReport.ReportStatus.FAILED ? 1 : 0;
+                boolean hasExceptions = exceptionDetails != null;
+                status = calculateStatus(qualityReport.getStatusCode(), hasExceptions, config.isPassBuildOnException());
             } catch(Exception e) {
                 // Report failed to be generated
                 context.getOutputStream().println("OverOps was unable to generate a Quality Report.", Ansi.Color.WHITE);
                 printError(e);
-                status = config.getReportParams().isMarkUnstable() ? 1 : 0;
+                status = calculateStatus(QualityReport.ReportStatus.FAILED, true, config.isPassBuildOnException());
             }
         } catch(Exception e) {
-            // Bad arguments
+            // Bad arguments. Very unlikely to happen and in this case the
+            // build will fail because we couldn't determine the users entered
+            // settings. Failure because status = 1 initially
             context.getOutputStream().println("OverOps was unable to generate a Quality Report.", Ansi.Color.WHITE);
             printError(e);
         }
 
         System.exit(status);
     }
+    
+    /**
+     * Determine whether to report the status of the build as pass or failed
+     *
+     * @param reportStatus
+     * @param passBuildOnException
+     * @return 1 for report failed or 0 if passed
+     */
+    private int calculateStatus(QualityReport.ReportStatus reportStatus, boolean hasExceptions, boolean passBuildOnException) {
+        int status = 1;
+        if (reportStatus == QualityReport.ReportStatus.FAILED) {
+            if (hasExceptions && passBuildOnException) {
+                status = 0;
+            }
+        } else {
+            status = 0;
+        }
 
+        return status;
+    }
+    
     private void printError(Exception e) {
         context.getOutputStream().printlnError("Exceptions: " + e.toString());
         context.getOutputStream().printlnError("Trace: " + Arrays.toString(e.getStackTrace()));
